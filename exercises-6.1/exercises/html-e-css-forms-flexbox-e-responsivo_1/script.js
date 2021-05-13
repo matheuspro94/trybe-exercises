69e5e7b3f7375e38c2aac30e8aa5cddc60878e72
Matheus Proenca <mproenca.marketing@gmail.com>
2021-05-13 09:36:09 -0300

    adicionando alguns estilos com bootstrap

--- exercises-6.1/exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
+++ exercises-6.1/exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -9,16 +9,24 @@ function criandoEstados(estados) {
 let estados = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO']
 criandoEstados(estados)
 
-// function verificaoData {
-//   let getDataInicial = document.getElementById('data-inicial');
-//   // dia
-//   // mes
-//   // ano, não pode ser negativo
-//   // exibir um alerta 
-
+// function verificaoDeData() {
   
-//   if (getDataInicial > 0 && <= 31 ) 
+//   getDataInicial.value.indexOf()
+//   // dia
+//   if (getDataInicial.indeOf() > 0 && <= 31 ){
+//     alert('Erro! dia in')
+//   }
+//   // mes
+//   if (getDataInicial.value > 0 && <= 12){
+//     alert('Erro digite uma data correta')
+//   }
+//   // ano
+//   if (getDataInicial.value === negativo){
+//     alert('Você não pode digitar um numero NEGATIVO!')
+//   }
 // }
+// let getDataInicial = document.getElementById('data-inicial').innerHTML;
+// verificaoDeData(getDataInicial)
 
 function stopSetButton(event) {
   event.preventDefault()
