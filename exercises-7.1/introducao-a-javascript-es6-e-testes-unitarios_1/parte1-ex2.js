3023ceb79a7af5fe532a476d06d63929ce84a256
Matheus Proenca <mproenca.marketing@gmail.com>
2021-05-20 17:57:53 -0300

    adiciona branch 7.2

--- exercises-7.1/introducao-a-javascript-es6-e-testes-unitarios_1/parte1-ex2.js
+++
@@ -1,9 +0,0 @@
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
-oddsAndEvens.sort((a, b) => a - b);
-
-console.log(oddsAndEvens);
-console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`)
-
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
-
-console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente !`);
--- exercises-7.1/introducao-a-javascript-es6-e-testes-unitarios_1/parte2-ex3/parte2-ex3.js
+++
@@ -1,5 +0,0 @@
-let contagemDeClique = 0;
-let textoQueVaiAparecer = document.querySelector('#texto');
-let btn = document.querySelector('#btn');
-
-btn.addEventListener("click", () => textoQueVaiAparecer.innerHTML = contagemDeClique += 1);
