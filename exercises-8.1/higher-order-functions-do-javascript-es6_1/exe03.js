9247c0299d3d6c15db42d5d3d02cfd2440272b97
Matheus Proenca <mproenca.marketing@gmail.com>
2021-05-25 21:19:57 -0300

    adiciona exer 01 - 02

---
+++ exercises-8.1/higher-order-functions-do-javascript-es6_1/exe03.js
@@ -0,0 +1,8 @@
+const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const respostaCorreta = (rightAnswers, studentAnswers) => {
+  
+}
+
+console.log(respostaCorreta())
