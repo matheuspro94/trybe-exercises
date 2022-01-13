19fc9c1d28b6b1f2c620d89a38225c482fa9084d
mroenca40 <mproenca.marketing@gmail.com>
2022-01-13 17:19:49 -0300

    adiciona repositorio 30.1 exemplo de mvc

---
+++ Bloco-30/30.1/mvc-example/controllers/authorController.js
@@ -0,0 +1,21 @@
+const Author = require('../models/Author');
+
+const listAuthors = async (req, res) => {
+ const authors = await Author.getAll();
+
+ return res.status(200).render('authors/index', { authors })
+};
+
+const showAuthor = async (req, res) => {
+  const { id } = req.params;
+  const author = await Author.findById(id);
+
+  if (!author) return res.status(404).render('authors/404');
+
+  res.status(200).render('authors/show', { author });
+};
+
+module.exports = {
+  listAuthors,
+  showAuthor
+}
---
+++ Bloco-30/30.1/mvc-example/models/Author.js
@@ -0,0 +1,59 @@
+const connection = require('./connection');
+
+// Cria uma string com o nome completo do autor
+
+const getNewAuthor = (authorData) => {
+  const { id, firstName, middleName, lastName } = authorData;
+
+  const fullName = [firstName, middleName, lastName]
+    .filter((name) => name)
+    .join(' ');
+
+  return {
+    id,
+    firstName,
+    middleName,
+    lastName,
+    name: fullName,
+  };
+};
+
+// Serializa o nome dos campos de snake_case para camelCase
+
+const serialize = (authorData) => ({
+  id: authorData.id,
+  firstName: authorData.first_name,
+  middleName: authorData.middle_name,
+  lastName: authorData.last_name});
+
+// Busca todos os autores do banco.
+
+const getAll = async () => {
+  const [authors] = await connection.execute(
+    'SELECT id, first_name, middle_name, last_name FROM authors;',
+  );
+  return authors.map(serialize).map(getNewAuthor);
+};
+
+const findById = async (id) => {
+  const [ authorData ] = await connection.execute(
+    'SELECT  first_name, middle_name, last_name FROM authors WHERE id = ?',
+    [id]
+  );
+
+  if (!authorData.length) return null;
+
+  const { firstName, middleName, lastName } = authorData.map(serialize)[0];
+
+  return getNewAuthor({
+    id,
+    firstName,
+    middleName,
+    lastName,
+  });
+};
+
+module.exports = {
+  getAll,
+  findById
+};
